859d7d9075779c285301f4094e43b31947af5d6f
Tim Robb <tim@robbmob.com>
2020-07-12 22:08:43 +1200

    Get basic dashboard layout setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include all devise modules.
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable, :trackable, :lockable, :omniauthable, :timeoutable
+  devise :database_authenticatable, :registerable, :recoverable, 
+    :rememberable, :validatable, :confirmable, 
+    :trackable, :lockable, :omniauthable, :timeoutable
 
   # Main Attributes: (excl. devise)
   # Email, Name
